38ac9925e1eb14bb7cad3e0dedcf62a0b923ea6f
Luccas Silva <lluccas.anjoss@gmail.com>
2022-03-13 12:42:58 -0300

    Product memory

--- src/main/java/com/JavaDev/MyFirstProject/entities/Product.java
+++ src/main/java/com/JavaDev/MyFirstProject/entities/Product.java
@@ -1,26 +1,25 @@
 package com.JavaDev.MyFirstProject.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-public class Category implements Serializable {
+public class Product implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
 	private String name;
+	private Double price;
 	
-	@JsonIgnore
-	private List<Product> products = new ArrayList<>();
+	private Category category;
 	
-	public Category () {
+	public Product () {
 		
 	}
-	public Category (Long id, String name) {
+	public Product(Long id, String name, Double price, Category category) {
+		super();
 		this.id = id;
 		this.name = name;
+		this.price = price;
+		this.category = category;
 	}
 
 	public Long getId() {
@@ -29,18 +28,28 @@ public class Category implements Serializable {
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	public List<Product> getProducts() {
-		return products;
+
+	public Double getPrice() {
+		return price;
 	}
-	
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -56,7 +65,7 @@ public class Category implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -64,5 +73,5 @@ public class Category implements Serializable {
 			return false;
 		return true;
 	}
-	
+
 }
